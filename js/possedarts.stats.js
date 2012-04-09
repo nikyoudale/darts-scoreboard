e61e4c98e25f8175f1e3f16df4da6ecf79034dd2
Michael Hills <mhills@gmail.com>
2012-04-09 15:30:26 +1000

    add possedarts.stats module and graphs

---
+++ js/possedarts.stats.js
@@ -0,0 +1,44 @@
+var possedartsStatsModule = angular.module('possedarts.stats', []);
+
+possedartsStatsModule.value('ready', false);
+
+/* This seems to need to run immediately, and can't run as part of the angular "run" configuration stage. */
+google.load('visualization', '1.0', {'packages':['corechart']});
+google.setOnLoadCallback(function() {
+    possedartsStatsModule.value('ready', true);
+});
+
+function GraphCtrl($scope, Player, PlayerScoresService) {
+
+  $scope.players = Player.getAll({}, function() {
+    $.each($scope.players, function(index, player) {
+      PlayerScoresService.refreshPlayerData(player.id);
+    });
+  });
+
+  $scope.drawChart = function(player) {
+    if (possedartsStatsModule.value('ready') == false) {
+      alert("Google Visualization API has not yet been loaded.");
+      return;
+    }
+    var scores = PlayerScoresService.playerScores(player.id);
+    var data = new google.visualization.DataTable();
+    data.addColumn('date', 'Date');
+    data.addColumn('number', 'Score');
+    for (var i = 0; i < scores.length; i++) {
+      data.addRow([localDateFromUTC(scores[i].date), scores[i].points]);
+    }
+
+    var options = {
+      'title': 'Dart throws over time',
+      'hAxis': {title: 'Time'},
+      'vAxis': {title: 'Score', minValue: 0, maxValue: 600},
+      'width': 600,
+      'height': 400};
+
+    var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+}
+GraphCtrl.$inject = ['$scope', 'Player', 'PlayerScoresService'];
+
